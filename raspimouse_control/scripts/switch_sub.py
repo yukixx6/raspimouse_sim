1178739142a275274d66e0996914852eba0e7cb3
yukixx6 <s1526707DA@s.chibakoudai.jp>
2017-11-14 00:37:52 +0900

    Add switch_sub.py

--- raspimouse_control/scripts/switch_sub.py
+++ raspimouse_control/scripts/switch_sub.py
@@ -1,14 +1,15 @@
+#!/usr/bin/env python
 import sys, rospy
 from raspimouse_ros.msg import Switches
 
 def switches_callback():
 		try:
 			with open(devfile + '0','w') as f:
-				'0' if vel.front = True else '1'
+				'0' if vel.front == True else '1'
 			with open(devfile + '1','w') as f:
-				'0' if vel.center = True else '1'
+				'0' if vel.center == True else '1'
 			with open(devfile + '2','w') as f:
-				'0' if vel.rear = True else '1'
+				'0' if vel.rear == True else '1'
 		except:
 			rospy.logerr("cannot write" + devfile + "[0,1,2]")
 
@@ -20,5 +21,6 @@ if __name__ == "__main__":
 	devfile = '/dev/rtswitch'
 	rospy.init_node("switches_data")
 	listener()
+	print vel
 	rospy.spin()
 
