d1224100fbb4742b254145fe9027d5fb2c4e17bb
yukixx6 <s1526707DA@s.chibakoudai.jp>
2017-11-22 19:54:19 +0900

    Update ledfiles

--- raspimouse_control/scripts/led_control.py
+++ raspimouse_control/scripts/led_control.py
@@ -1,48 +1,50 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from raspimouse_ros_2.msg import LedValues
 
 if __name__ == "__main__":
 	rospy.init_node('led_control')
-	pub = rospy.Publisher('/raspimouse_on_gazebo/leds', String, queue_size=10)
+	pub = rospy.Publisher('/raspimouse_on_gazebo/leds', LedValues, queue_size=10)
 	try:
 		while not rospy.is_shutdown():
-			cmd = String()
-			cmd.data = [0,0,0,0]
-			direction = raw_input('[ LED0 LED1 LED2 LED3 ] > ')
+			vel = LedValues()
+			print ("ex) LED0 > on")
+			direction0 = raw_input(' LED0 > ')
+			direction1 = raw_input(' LED1 > ')
+			direction2 = raw_input(' LED2 > ')
+			direction3 = raw_input(' LED3 > ')
 
-			if '1' in direction[0]:
-				cmd.data[0] = 1
-			elif '0' in direction[0]:
-				cmd.data[0] = 0
+			if 'on' in direction0 or '1' in direction0:
+				vel.right_side = True
+			elif 'off' in direction0 or '0' in direction0:
+				vel.right_side = False
 			else:
 				rospy.logerr("cannot write LED0" + "\n")
 
-			if '1' in direction[1]:
-				cmd.data[1] = 1
-			elif '0' in direction[1]:
-				cmd.data[1] = 0
+			if 'on' in direction1 or '1' in direction1:
+				vel.right_forward = True
+			elif 'off' in direction1 or '0' in direction1:
+				vel.right_forward = False
 			else:
 				rospy.logerr("cannot write LED1" + "\n")
 
-			if '1' in direction[2]:
-				cmd.data[2] = 1
-			elif '0' in direction[2]:
-				cmd.data[2] = 0
+			if 'on' in direction2 or '1' in direction2:
+				vel.left_forward = True
+			elif 'off' in direction2 or '0' in direction2:
+				vel.left_forward = False
 			else:
 				rospy.logerr("cannot write LED2" + "\n")
 
-			if '1' in direction[3]:
-				cmd.data[3] = 1
-			elif '0' in direction[3]:
-				cmd.data[3] = 0
+			if 'on' in direction3 or '1' in direction3:
+				vel.left_side = True
+			elif 'off' in direction3 or '0' in direction3:
+				vel.left_side = False
 			else:
-				rospy.logerr("cannot write LED3" + "\n")
+				rospy.logerr("cannot write LED0" + "\n")
 
-			cmd.data = str(cmd.data)
-			print cmd
+			print vel
 			print "\n"
-			pub.publish(cmd)
+			pub.publish(vel)
 
 	except rospy.ROSInterruptException:
 		pass
--- raspimouse_control/scripts/led_sub.py
+++ raspimouse_control/scripts/led_sub.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from raspimouse_ros_2.msg import LedValues
 
 devfile = '/dev/rtled'
 
 def leds_callback(data):
 	try:
-		
 		with open(devfile + '0','w') as f:
-			f.write(data.data[1] + "\n")
+			f.write('1' + "\n") if data.right_side == True else f.write('0' + "\n")
 		with open(devfile + '1','w') as f:
-			f.write(data.data[4])
+			f.write('1' + "\n") if data.right_forward == True else f.write('0' + "\n")
 		with open(devfile + '2','w') as f:
-			f.write(data.data[7])
+			f.write('1' + "\n") if data.left_forward == True else f.write('0' + "\n")
 		with open(devfile + '3','w') as f:
-			f.write(data.data[10])
+			f.write('1' + "\n") if data.left_side == True else f.write('0' + "\n")
 	except:
 		rospy.logerr("cannot write" + devfile + "[0,1,2,3]")
 
 if __name__ == "__main__":
 	rospy.init_node("led_data")
-	rospy.Subscriber("/raspimouse_on_gazebo/leds", String, leds_callback)
+	rospy.Subscriber("/raspimouse_on_gazebo/leds", LedValues, leds_callback)
 	rospy.spin()
